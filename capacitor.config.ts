f05d3794b0e84c5bd4ac55d41a9fa673f59ea47e
sacbabylandiabras-rgb <sac.babylandiabras@gmail.com>
2026-04-08 17:41:08 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.9023f99095c6410f8f3bbb7c314444a5',
-  appName: 'talkweave-pro',
+  appId: 'com.zaplynxpay.app',
+  appName: 'Zaplynx Pay',
   webDir: 'dist',
   server: {
     url: 'https://9023f990-95c6-410f-8f3b-bb7c314444a5.lovableproject.com?forceHideBadge=true',
